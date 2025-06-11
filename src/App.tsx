1f85efc9b6ed63e575d152b37975cc7f936b50c7
WebsitesArmando45 <armandoflowbuilds@hotmail.com>
2025-06-11 15:25:12 +0200

    Move Process section after Services

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Services from './components/Services';
+import Process from './components/Process';
+import Portfolio from './components/Portfolio';
+import About from './components/About';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <div className="bg-white">
+      <Header />
+      <Hero />
+      <Services />
+      <Process />
+      <Portfolio />
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
